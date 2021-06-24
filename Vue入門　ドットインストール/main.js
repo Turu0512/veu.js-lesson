cc0c33822de333a563ff7a015b23c90dddb8c5e2
Turu0512 <hinataptyan@gmail.com>
2021-06-24 20:23:02 +0900

    データ入力の処理　prevent

--- Vue入門　ドットインストール/main.js
+++ Vue入門　ドットインストール/main.js
@@ -7,11 +7,22 @@
 let vm = new Vue({
 el:'#app',
 data: {
+  newItem:'',
   todos: [
     'task 1',
     'task 2',
     'task 3'
   ]
+},
+methods: {
+  // addItem: function(e){
+  //   e.preventDefault();文の遷移（初期状態に戻る、リロードされる）を止める
+  //   this.todos.push(this.newItem);
+  addItem: function(){
+    this.todos.push(this.newItem);
+    this.newItem = ""
+    // Boxの中を空にする
+  }
 }
 
 });
