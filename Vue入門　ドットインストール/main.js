25075014545f6150952df5defd8a872d4cc422b7
Turu0512 <hinataptyan@gmail.com>
2021-06-24 19:40:16 +0900

    Vueの説明　双方向性

---
+++ Vue入門　ドットインストール/main.js
@@ -0,0 +1,14 @@
+(function(){
+'use strict';
+//two way data binding(to UI)
+// 双方向データバインディング（データとUIを結びつける）
+//データを更新すればUIも更新され、UIを更新すればデータも更新されるという考え
+
+let vm = new Vue({
+el:'#app',
+data: {
+  name: 'taguchi'
+}
+
+});
+})();
