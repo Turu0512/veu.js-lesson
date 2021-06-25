6e78e78fcdb2732f5e0f7d37eadf5ecc89d03a36
Turu0512 <hinataptyan@gmail.com>
2021-06-26 05:33:49 +0900

    Propsで値を渡す方法

--- Vue入門　ドットインストール/Componentについて/main.js
+++ Vue入門　ドットインストール/Componentについて/main.js
@@ -4,14 +4,22 @@
   //Components
 
   let likeComponent = Vue.extend({
-data:function(){
+    // props:["message"],
+    props:{
+      message:{
+        type:String,
+        default:"Like"
+      }
+    },
+
+  data:function(){
   return{
     count:0
   }
 },
 // コンポーネントでdataを扱う際には関数で返してあげる
 
-    template:'<button @click="countUp">like{{count}}</button>',
+    template:'<button @click="countUp">{{message}}{{count}}</button>',
     // template:'<button>like</button><button>like</button>'二つは入れれない。するならdivなどの親要素で囲む
 methods: {
   countUp: function(){
