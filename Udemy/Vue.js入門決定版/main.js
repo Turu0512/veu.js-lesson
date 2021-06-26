7b893d9cba8c93bef2428a325588422e015a0386
Turu0512 <hinataptyan@gmail.com>
2021-06-27 08:55:46 +0900

    双方向　v-model　

--- Udemy/Vue.js入門決定版/main.js
+++ Udemy/Vue.js入門決定版/main.js
@@ -1,12 +1,7 @@
 var app = new Vue({
   el: '#app',
   data: {
-    now:''
-    },
-    methods:{
-    onclick:function(){
-    /* alert('onclick'); */
-    this.now=new Date().toLocaleString();
+    message:'Hello Vue.js'
     }
-  }
+  
 })
