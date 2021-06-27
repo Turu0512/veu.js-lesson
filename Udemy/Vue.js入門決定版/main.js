d7c3905f9c91231fcdd43bcc8acfe88c51935cf6
Turu0512 <hinataptyan@gmail.com>
2021-06-27 09:01:33 +0900

    component　の使い方

--- Udemy/Vue.js入門決定版/main.js
+++ Udemy/Vue.js入門決定版/main.js
@@ -1,7 +1,11 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    message:'Hello Vue.js'
-    }
+Vue.component('hello-component',{
+  template:'<p>Hello</p>'
+  })
   
-})
+  var app = new Vue({
+    el: '#app',
+    data: {
+      message:'Hello Vue.js'
+      
+    }
+  })
