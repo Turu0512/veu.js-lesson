ed33eeb5b36ff4500bf09aed00f7e1fd0d5d03dd
Turu0512 <hinataptyan@gmail.com>
2021-06-27 15:41:54 +0900

    通信エラーハンドリング

--- Udemy/Vue.js入門決定版/Bitcoin監視アプリ/main.js
+++ Udemy/Vue.js入門決定版/Bitcoin監視アプリ/main.js
@@ -2,14 +2,25 @@ let app = new Vue({
   el:'#app',
   data: {
   bpi:null,
+  hasError:false,
   },
+  
   mounted:function() {
 axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then(function(response){
   // console.log(response.data.bpi)
   this.bpi = response.data.bpi
 }.bind(this))
+
 .catch(function(error){
   console.log(error)
-})
+  this.hasError = true
+}.bind(this))
+
+  },
+
+  filters:{
+    currencyDecimal(value){
+      return value.toFixed(2)
+    }
   }
 })
