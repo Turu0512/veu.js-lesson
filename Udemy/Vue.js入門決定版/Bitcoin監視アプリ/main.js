6648c0731efc0bcb1ebd42204330e9a553ec152b
Turu0512 <hinataptyan@gmail.com>
2021-06-27 14:39:09 +0900

    サーバーからの情報取得

---
+++ Udemy/Vue.js入門決定版/Bitcoin監視アプリ/main.js
@@ -0,0 +1,12 @@
+let app = new Vue({
+  el:'#app',
+  data: {
+  bpi:null,
+  },
+  mounted:function() {
+axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then(function(response){
+  // console.log(response.data.bpi)
+  this.bpi = response.data.bpi
+}.bind(this))
+  }
+})
