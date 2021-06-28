764a2b8f30711959bdc2e0e5e74953aae4f6807a
Turu0512 <hinataptyan@gmail.com>
2021-06-28 21:45:50 +0900

    deepについて

--- Udemy/Vue.js入門決定版/監視プロパティー/main.js
+++ Udemy/Vue.js入門決定版/監視プロパティー/main.js
@@ -1,33 +1,18 @@
-// 監視プロパティー
-// let app = new Vue({
-//   el:'#app',
-//   data:{
-//     firstName:'',
-//     lastName:'',
-//     fullName:'',
-//   },
-//   watch:{
-//     firstName:function(value){
-//       this.fullName = value + ' '+this.lastName
-//   },
-//   lastName:function(value){
-//     this.fullName=this.firstName +' '+value
-//   }
-//   }
-//   })
-
-// 算出プロパティー
-
 let app = new Vue({
   el:'#app',
   data:{
-    firstName:'',
-    lastName:'', 
+  colors:[
+    { name: 'Red'},
+    { name: 'Green'},
+    { name: 'Blue'}
+  ]
   },
-  computed: {
-    fullName:function(){
-      return this.firstName+' '+this.lastName
+  watch:{
+    colors:{
+      handler:function(newValue, oldValue){
+        console.log('Update!')
+      },
+      deep:true
     }
   }
-
   })
