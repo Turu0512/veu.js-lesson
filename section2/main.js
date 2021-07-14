effa7971a6ab9ac7f9506ff491eb21d2457c5b37
Turu0512 <hinataptyan@gmail.com>
2021-07-15 06:18:40 +0900

    thisを使う

--- section2/main.js
+++ section2/main.js
@@ -2,12 +2,11 @@ new Vue({
   el:'#app',
   data:{
     message: 'hello world',
-    number:3,
-    ok:true,
+    
   },
   methods: {
     sayHi:function(){
-      return 'hi';
+      return this.message;
     }
     },
 })
