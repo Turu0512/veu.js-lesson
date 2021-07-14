78a92abfc7ec03461810e2ac2020b25c78b09b52
Turu0512 <hinataptyan@gmail.com>
2021-07-15 06:46:48 +0900

    v-bindの省略技法

--- section2/main.js
+++ section2/main.js
@@ -1,6 +1,13 @@
 new Vue({
  el:'#app',
  data:{
-  url:'https://google.com'
- },
+  url:'https://google.com',
+  // urlTwitter: 'https://twitter.com',
+  // number:'31',
+  attribute:"href",
+  twitterObject:{
+   href: 'https://twitter.com',
+   number: '31'
+  },
+ }
 })
