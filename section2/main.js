2c129db6086a9144bb27809759f950f652ec4c35
Turu0512 <hinataptyan@gmail.com>
2021-07-15 07:22:07 +0900

    v-onの使い方

--- section2/main.js
+++ section2/main.js
@@ -1,13 +1,11 @@
 new Vue({
  el:'#app',
  data:{
-  url:'https://google.com',
-  // urlTwitter: 'https://twitter.com',
-  // number:'31',
-  attribute:"href",
-  twitterObject:{
-   href: 'https://twitter.com',
-   number: '31'
-  },
+  count:0
+ },
+ methods:{
+  countUp:function(){
+   this.count++
+  }
  }
 })
