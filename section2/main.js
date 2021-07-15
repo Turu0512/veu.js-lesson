a7a732d92601a3f3e75f5a886c9b8668967288e9
Turu0512 <hinataptyan@gmail.com>
2021-07-16 06:06:08 +0900

    methodsとcomputedの違い

--- section2/main.js
+++ section2/main.js
@@ -1,6 +1,20 @@
 new Vue({
  el:'#app',
  data:{
-  message:'こんにちわ'
- }
+  counter:0, 
+  othercounter:0
+ },
+ computed:{
+  lessThanThreeComputed:function(){
+   console.log('computed')
+   return this.counter > 3? '3より上' : '３以下'
+  },
+ },
+  methods: {
+   lessThanThreeMethod:function(){
+    console.log('methods')
+    return this.counter > 3? '3より上' : '３以下'
+   }
+
+  }
  })
