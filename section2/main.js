afe97baf30f07d7893e063dcaf08e4821ade10fd
Turu0512 <hinataptyan@gmail.com>
2021-07-16 05:32:36 +0900

    v-model

--- section2/main.js
+++ section2/main.js
@@ -1,21 +1,6 @@
 new Vue({
  el:'#app',
  data:{
-  count:0,
-  x:0,
-  y:0,
-  event:'click',
- },
- methods:{
-  countUp:function(time){
-   this.count+=1*time
-  },
-  changeMousePosition:function(event , divide){
-    this.x = event.clientX/divide;
-    this.y = event.clientY/divide;
-   },
-  myAlert(){
-    alert('アラート')
-   }
-  }
+  message:'こんにちわ'
+ }
  })
