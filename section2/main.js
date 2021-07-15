3080014bdb5b3ffe384fb77a0a906fcc04033f80
Turu0512 <hinataptyan@gmail.com>
2021-07-15 21:04:54 +0900

    引数を持ったv-on

--- section2/main.js
+++ section2/main.js
@@ -1,11 +1,17 @@
 new Vue({
  el:'#app',
  data:{
-  count:0
+  count:0,
+  x:0,
+  y:0,
  },
  methods:{
-  countUp:function(){
-   this.count++
-  }
+  countUp:function(time){
+   this.count+=1*time
+  },
+  changeMousePosition:function(event , divide){
+    this.x = event.clientX/divide;
+    this.y = event.clientY/divide;
+   },
  }
-})
+ })
