e0f1069a7fa6d561e28b87a1ccb3ebb3a1a0b980
Turu0512 <hinataptyan@gmail.com>
2021-07-16 19:11:28 +0900

    データバインディングの配列での指定

--- section2/main.js
+++ section2/main.js
@@ -1,22 +1,17 @@
 new Vue({
  el:'#app',
  data:{
-  counter:0, 
-  othercounter:0
+  isActive:true,
+  color:'red',
+  bg:"bg-blue"
  },
+
  computed:{
-  lessThanThreeComputed:function(){
-   console.log('computed')
-   return this.counter > 3? '3より上' : '３以下'
-  },
- },
- watch:{
-  // 非同期処理に使う。それ以外は基本computed
-  counter:function(){
-   let vm =this
-   setTimeout(function(){
-    vm.counter=0
-   },2000)
+  classObject:function(){
+   return {
+   red:this.isActive,
+   'bg-blue':!this.isActive
+  }
  }
-}
+ }
  })
