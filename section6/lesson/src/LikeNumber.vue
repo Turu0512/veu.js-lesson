a25c2b7c05d50c12387bd80dd2fa18c1f0bc6787
Turu0512 <hinataptyan@gmail.com>
2021-07-18 07:35:34 +0900

    グローバルコンポーネントの使い方

---
+++ section6/lesson/src/LikeNumber.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  <p>いいね（{{number}}）</p>
+  <button @click="incriment">+1</button>
+    
+  </div>
+</template>
+
+<script>
+export default{
+  data:function(){
+    return {
+      number:5
+    }
+  },
+  methods:{
+    incriment:function(){
+      this.number++
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
