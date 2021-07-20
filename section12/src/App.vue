8999e456bc3194670fcd9c9824a1f729f8c22dcb
Turu0512 <hinataptyan@gmail.com>
2021-07-21 05:55:11 +0900

    トランジションとアニメーション

---
+++ section12/src/App.vue
@@ -0,0 +1,70 @@
+<template>
+<div class="main">
+<button @click="show=!show">切り替え</button>
+<transition name="fade">
+  <p v-if="show">hello</p>
+</transition>
+<transition name="slide">
+  <p v-if="show">hello</p>
+</transition>
+</div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+        show: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.fade-enter {
+  /* 現れる時の状態 */
+  opacity:0;
+}
+.fade-enter-active{
+/* 現れる時のトランジションの状態 */
+transition: opacity 0.5s;
+}
+.fade-enter-to{
+/* 現れる時の最後の状態 */
+opacity:1;
+}
+.fade-leave{
+  /* 消える時の状態 */
+  opacity: 1;
+}
+.fade-leave-active{
+  /* 消える時のトランジションの状態 */
+  transition: opacity 0.5s;
+}
+.fade-leave-to{
+  /* 消える時の最後の状態 */
+  opacity: 0;
+}
+
+.slide-enter-active{
+  animation:slide-in 0.5s;
+}
+.slide-leave-active{
+  animation:slide-in 0.5s reverse;
+}
+
+@keyframes slide-in{
+  from{
+    transform: translateX(100px);
+  }
+  to{
+    transform: translateX(0);
+  }
+}
+.main {
+  width: 70%;
+  margin: auto;
+  padding: 5rem;
+  text-align:center;
+}
+</style>
