2a4c60d4ecdbc0df4a0a270977a53f9a3219d5c2
Turu0512 <hinataptyan@gmail.com>
2021-07-20 16:14:44 +0900

    フィルターの使い方（upperCase)

--- section9/src/components/Home.vue
+++ section9/src/components/Home.vue
@@ -1,4 +1,29 @@
 <template>
+<div>
 <p　v-border:solid.shadow.round="{width:'5px' , color:'red'}">Home</p>
-
+<h2>{{title | upperCase}}</h2>
+<p>{{subTitle | upperCase}}</p>
+</div>
 </template>
+
+<script>
+export default{
+ data(){
+  return{
+   title: "Welcome to Tokyo",
+   subTitle:"Tokyo is a great"
+  }
+ },
+ directives:{
+  border(el,binding){
+ el.style.borderWidth = binding.value.width
+ el.style.borderColor = binding.value.color
+ el.style.borderStyle = binding.arg
+  if(binding.modifiers.round){
+   el.style.borderRadius ="0.5rem"}
+  if(binding.modifiers.shadow){
+   el.style.boxShadow ="0 2px 5px rgba(0,0,0,0.5)"}
+ }
+ }
+}
+</script>
