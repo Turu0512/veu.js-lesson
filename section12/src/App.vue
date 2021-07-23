6a4a783e9a2c023d9b40f8bc9d7951068588fca5
Turu0512 <hinataptyan@gmail.com>
2021-07-23 19:53:50 +0900

    終了

--- section12/src/App.vue
+++ section12/src/App.vue
@@ -3,7 +3,25 @@
   <button @click="myAnimation='slide'">slide</button>
   <button @click="myAnimation='fade'">fade</button>
   <p>{{myAnimation}}</p>
+
+  <br>
+  <button @click="myComponent='myComponentA'">myComponentA</button>
+  <button @click="myComponent='myComponentB'">myComponentB</button>
+  <transition name="fade" mode="out-in">
+  <component :is="myComponent"></component>
+  </transition>
+
 <button @click="show=!show">切り替え</button>
+<br><br>
+<transition
+:css="false"
+@before-enter="beforeEnter"
+@enter="enter"
+@leave="leave"
+>
+  <div class="circle" v-if="show"></div>
+</transition>
+
 <transition name="fade" mode="out-in">
 <p v-if="show" key="bye">さよなら</p>
 <p v-else key="hello">こんにちは</p>
@@ -21,17 +39,67 @@
 </template>
 
 <script>
+import myComponentA from './components/myComponentA.vue'
+import myComponentB from './components/myComponentB.vue'
+
+
 export default {
+  components:{
+    myComponentA,
+    myComponentB
+  },
   data(){
     return {
         show: true,
         myAnimation: 'slide',
+        myComponent:'myComponentA',
     }
+  },
+  methods:{
+    beforeEnter(el){
+      // 現れる前
+      
+    },
+    enter(el,done){
+      // 現れる時
+      let scale = 0;
+      const interval= setInterval(()=>{
+        el.style.transform = `scale(${scale})`;
+        scale +=0.1;
+        if(scale>1){
+          clearInterval(interval);
+          done();
+        }
+        // cssが無い場合は「必ず！done」をつける
+      },20)
+    },
+    leave(el,done){
+      // 消える時
+      let scale = 1;
+      const interval= setInterval(()=>{
+        el.style.transform = `scale(${scale})`;
+        scale -=0.1;
+        if(scale<0){
+          clearInterval(interval)
+          done();
+        };
+        // cssが無い場合は「必ず！done」をつける
+      },20)
+    },
   }
 }
 </script>
 
 <style scoped>
+.circle{
+  height:200px;
+  width:200px;
+  margin:auto;
+  border-radius: 100%;
+  background-color: deeppink;
+}
+
+
 .fade-enter {
   /* 現れる時の状態 */
   opacity:0;
