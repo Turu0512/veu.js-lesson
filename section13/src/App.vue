4b7527435b60fc6edff75639276b324842c8184e
Turu0512 <hinataptyan@gmail.com>
2021-07-24 06:31:34 +0900

    名前付きViewを使って、Headerを固定する

--- section13/src/App.vue
+++ section13/src/App.vue
@@ -1,19 +1,7 @@
 <template>
   <div style="width:200px; margin:auto; padding-top:50px;">
-    <nav>
-      <router-link to="/" class="link" exact active-class="link--active">Home</router-link>
-      <router-link to="/users" class="link" exact active-class="link--active">Usues</router-link> 
-    </nav>
+    <router-view name="header"></router-view>
     <router-view></router-view>
   </div>
 </template>
 
-<style scoped>
-.link{
-  margin-right: 10px;
-}
-
-.link--active{
-  font-size: 20px;
-}
-</style>
---
+++ section13/src/views/HeaderUser.vue
@@ -0,0 +1,17 @@
+<template>
+ <nav>
+  <h2>Users</h2>
+  <router-link to="/users" class="link" exact active-class="link--active">Usues</router-link> 
+  <router-link to="/" class="link" exact active-class="link--active">Home</router-link>
+ </nav>
+</template>
+
+<style scoped>
+.link{
+  margin-right: 10px;
+}
+
+.link--active{
+  font-size: 20px;
+}
+</style>
