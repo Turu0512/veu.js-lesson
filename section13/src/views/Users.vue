e60bfc1d3a9c93db33c20cec8faf0089a90bce5c
Turu0512 <hinataptyan@gmail.com>
2021-07-24 06:07:27 +0900

    名前付きルート

--- section13/src/views/Users.vue
+++ section13/src/views/Users.vue
@@ -7,6 +7,8 @@
   <hr>
   <!-- <h1>User No.{{ $route.params.id }}</h1> -->
   <h1>User No.{{ id }}</h1>
+  <!-- <router-link :to="'/users/' + (Number(id) + 1) +'/profile'">次のユーザーへ</router-link> -->
+  <router-link :to="{name: 'users-id-profile', params: {id:Number(id)+1}}">次のユーザーへ</router-link>
   <router-view></router-view>
  </div>
 </template>
